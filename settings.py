9274ce4457b21a74e42f3229fc47531e790e039d
Emil Diaz <lcsantibanez.chmsu@gmail.com>
2026-01-10 22:38:29 +0800

    Add Mailtrap environment

--- settings.py
+++ settings.py
@@ -1,4 +1,6 @@
+
 import os
+from decouple import config
 
 # Enable debugging temporarily for detailed error messages
 DEBUG = True
@@ -30,3 +32,13 @@ DATABASES = {
 # Static files settings
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+# Mailtrap SMTP configuration for real email delivery (from .env)
+EMAIL_BACKEND = config('EMAIL_BACKEND', default='django.core.mail.backends.smtp.EmailBackend')
+EMAIL_HOST = config('EMAIL_HOST', default='sandbox.smtp.mailtrap.io')
+EMAIL_HOST_USER = config('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
+EMAIL_PORT = config('EMAIL_PORT', cast=int, default=2525)
+EMAIL_USE_TLS = config('EMAIL_USE_TLS', cast=bool, default=True)
+DEFAULT_FROM_EMAIL = config('DEFAULT_FROM_EMAIL', default='Skinovation Beauty Clinic <noreply@skinovation.com>')
+MAILTRAP_API_TOKEN = config('MAILTRAP_API_TOKEN', default='')
